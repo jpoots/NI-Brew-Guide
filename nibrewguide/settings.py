26ed62bb51f20701dd83a4c3df54cf4b8b187c9c
Jordan Poots <jordan.poots@outlook.com>
2023-01-06 15:31:13 +0000

    Changed settings

--- nibrewguide/settings.py
+++ nibrewguide/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@9^m@@uqtcxdk0w%7i1lhx(dfvm00b&#=0+5@$qtvliop=u14#'
+SECRET_KEY = 'removed for safety'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
